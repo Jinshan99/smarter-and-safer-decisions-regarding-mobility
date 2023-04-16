a4caae1f22c009a5562fa5ec440c6686e1e5406c
srajoo <103288051+srajoo@users.noreply.github.com>
2023-04-16 00:15:27 -0700

    Add files via upload

---
+++ src/pages/Landing/LandingPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationBar } from '../../utils';
+
+
+class LandingPage extends React.Component {
+    render () {
+        return (
+            <div id='main'>
+                <div id='header'>
+                    <NavigationBar />
+                </div>
+
+                <div id='body'>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
